e854e5f1ebaf656ddd7ebf39e90f5fa8d55a8036
acol248 <alex.collyer@acol.dev>
2023-12-24 11:37:51 +0000

    Updated ListItem

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -5,11 +5,21 @@ import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
 import maps from "./ListItem.module.scss";
 const mc = mapClassesCurried(maps, true);
 
-export default function ListItem({ className, variant, open, onToggle, name, value, children, ...props }) {
+export default function ListItem({ className, variant, open, onToggle, name, value, date, children, ...props }) {
   const classList = useClassList(
     { defaultClass: "list-item", className, variant, maps, string: true },
     useCallback(_c => open && _c.push("list-item--open"), [open])
   );
+
+  /**
+   * Append the appropriate suffix for dates from the gregorian calendar
+   *
+   * @param {number} day numerical representation of current day
+   * @returns {string} current day with appropriate suffix
+   */
+  const appendSuffix = day => {
+    return `${day}${day % 100 >= 11 && day % 100 <= 13 ? "th" : ["st", "nd", "rd", "th"][(day % 10) - 1]}`;
+  };
 
   const handleToggle = e => {
     e.preventDefault();
@@ -22,8 +32,12 @@ export default function ListItem({ className, variant, open, onToggle, name, val
   return (
     <details {...props} className={classList} open={open}>
       <summary onClick={handleToggle}>
-        <span className={mc("list-item__value")}>£{value}</span>
-        {name}
+        <div className={mc("list-item__left")}>
+          <span className={mc("list-item__value")}>£{value}</span>
+          {name}
+        </div>
+
+        <h6 className={mc("list-item__date")}>{date ? appendSuffix(new Date(date)?.getDay()) : "--"}</h6>
       </summary>
 
       <div className={mc("list-item__body")}>{children}</div>
