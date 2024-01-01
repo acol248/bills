cced0bb4f077b22b9b80510b1444156d99ec622f
acol248 <acol248@gmail.com>
2024-01-01 12:55:45 +0000

    🚧 FloatingMenu component

---
+++ src/components/FloatingMenu/FloatingMenu.jsx
@@ -0,0 +1,71 @@
+import { useLayer } from "react-laag";
+
+// components
+import Icon from "./FloatingMenu.icons";
+
+// styles
+import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
+import maps from "./FloatingMenu.module.scss";
+const mc = mapClassesCurried(maps, true);
+
+export default function FloatingMenu({ className, buttons }) {
+  const buttonRef = useRef(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { triggerProps, layerProps, renderLayer } = useLayer({
+    isOpen,
+    onOutsideClick: () => setIsOpen(false),
+    onDisappear: () => setIsOpen(false),
+    placement: "top-end",
+    overflowContainer: true,
+    triggerOffset: 5,
+    containerOffset: 10,
+    container: () => buttonRef.current.parentElement,
+  });
+
+  const buttonClassList = useClassList(
+    { defaultClass: "button", className, maps, string: true },
+    useCallback(c => isOpen && c.push("button--open"), [isOpen])
+  );
+  const menuClassList = useClassList({ defaultClass: "menu", maps, string: true });
+
+  /**
+   * Tidy stuff in this component when a button function is run
+   *
+   * @param {Function} callback callback function
+   */
+  const handleFunc = callback => {
+    setIsOpen(false);
+
+    callback && callback();
+  };
+
+  return (
+    <>
+      <button
+        {...triggerProps}
+        type="button"
+        aria-label="toggle floating menu"
+        className={buttonClassList}
+        ref={r => {
+          buttonRef.current = r;
+          triggerProps.ref(r);
+
+          return r;
+        }}
+        onClick={() => setIsOpen(o => !o)}
+      >
+        <Icon />
+      </button>
+
+      {isOpen &&
+        renderLayer(
+          <div {...layerProps} className={menuClassList}>
+            {Boolean(buttons.length > 0) &&
+              buttons.map(({ label, func }) => <button onClick={() => handleFunc(func)}>{label}</button>)}
+          </div>
+        )}
+    </>
+  );
+}
