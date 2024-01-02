6f30085f1e6c574218a2982d312ec6847d6456fd
acol248 <acol248@gmail.com>
2024-01-02 18:22:31 +0000

    Updated Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,7 +11,16 @@ import Icon from "./Modal.icons";
 import maps from "./Modal.module.scss";
 const mc = mapClassesCurried(maps, true);
 
-export default function Modal({ className, variant, title, open, onClose, children, locked, onTransitionEnd }) {
+export default function Modal({
+  className,
+  variant,
+  title,
+  open,
+  onClose = () => {},
+  children,
+  locked = false,
+  onTransitionEnd,
+}) {
   const { useVibration } = useContext(SettingsContext);
 
   const modalRef = useRef(null);
@@ -21,31 +30,55 @@ export default function Modal({ className, variant, title, open, onClose, childr
     useCallback(_c => !open && _c.push("modal--closing"), [open])
   );
 
-  // Eventlistener: trigger onclose when cancel detected
+  /**
+   * Eventlistener: trigger close when close modal event
+   */
+  const handleClose = useCallback(e => {
+    e?.preventDefault();
+    e?.stopPropagation();
+
+    onClose();
+  }, []);
+
+  /**
+   * Eventlistener: trigger onclose when cancel detected
+   */
   const onCancel = useCallback(
     e => {
       e.preventDefault();
-      if (!locked) onClose();
+
+      if (!locked) handleClose(e);
     },
-    [locked, onClose]
+    [locked, handleClose]
   );
 
-  // Eventlistener: trigger onclose when click outside
+  /**
+   * Eventlistener: trigger onclose when click outside
+   */
   const onClick = useCallback(
-    ({ target }) => {
+    e => {
       const { current: el } = modalRef;
-      if (target === el && !locked) onClose();
+      if (e.target === el && !locked) handleClose(e);
     },
-    [locked, onClose]
+    [locked, handleClose]
   );
 
-  // Eventlistener: trigger close click on anim end
-  const onAnimEnd = useCallback(() => {
-    const { current: el } = modalRef;
-    if (!open) el.close();
+  /**
+   * Eventlistener: trigger close click on anim end
+   */
+  const onAnimEnd = useCallback(
+    e => {
+      e.preventDefault();
+      e.stopPropagation();
 
-    onTransitionEnd && onTransitionEnd();
-  }, [open]);
+      const { current: el } = modalRef;
+
+      if (!open) el.close();
+
+      onTransitionEnd && onTransitionEnd();
+    },
+    [open]
+  );
 
   // when open changes run open/close command
   useEffect(() => {
@@ -56,7 +89,7 @@ export default function Modal({ className, variant, title, open, onClose, childr
   return (
     <dialog
       className={classList}
-      onClose={onClose}
+      onClose={handleClose}
       onCancel={onCancel}
       onClick={onClick}
       onAnimationEnd={onAnimEnd}
@@ -66,7 +99,7 @@ export default function Modal({ className, variant, title, open, onClose, childr
       <div className={mc("modal__header")}>
         <h2 className={mc("modal__title")}>{title}</h2>
 
-        <button className={mc("modal__close")} onClick={() => useVibration(8, () => onClose())}>
+        <button className={mc("modal__close")} onClick={e => useVibration(8, () => handleClose(e))}>
           <Icon type="close" />
         </button>
       </div>
