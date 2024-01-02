5af0a386cee06b3f355a74d269caaf45065cb7ad
acol248 <alex.collyer@acol.dev>
2024-01-02 15:19:25 +0000

    Improved MenuButton

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -1,14 +1,14 @@
 import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
 
 // maps
-import maps from "./MenuItem.module.scss";
+import maps from "./MenuButton.module.scss";
 const mc = mapClassesCurried(maps, true);
 
-export default function MenuItem({ className, variant, children }) {
+export default function MenuItem({ className, variant, children, ...props }) {
   const classList = useClassList({ defaultClass: "menu-button", className, variant, maps, string: true });
 
   return (
-    <button className={classList}>
+    <button className={classList} {...props}>
       <p className={mc("menu-button__content")}>{children}</p>
     </button>
   );
