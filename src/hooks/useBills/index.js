5f614e63b7dfb58ce7c5ff56575a19455aa17a15
acol248 <acol248@gmail.com>
2024-01-02 18:23:43 +0000

    ✨ Updated bills hook

--- src/hooks/useBills/index.js
+++ src/hooks/useBills/index.js
@@ -8,20 +8,35 @@ export default function useBills() {
   const initialised = useRef(false);
   const _storageList = localStorage.getItem("bills");
 
-  const [bills, setBills] = useState(_storageList ? decodeBase64(_storageList) : { default: [] });
+  const [bills, setBills] = useState(_storageList ? decodeBase64(_storageList) : {});
 
   /**
    * Add new bill to list
    *
    * @param {object} payload name and value of bill
    */
-  const addBill = (payload, category = "default") => {
-    setBills(b => ({
-      ...b,
-      ...(b[category]
-        ? { [category]: [...b[category], { id: generateUniqueId(16), ...payload }] }
-        : { [category]: [{ id: generateUniqueId(16), ...payload }] }),
-    }));
+  const addBill = (payload, list = "default") => {
+    setBills(b => {
+      if (!b[list]) return b;
+
+      return { ...b, [list]: [...b[list], { ...payload, id: generateUniqueId(16) }] };
+    });
+  };
+
+  /**
+   * Add new list
+   *
+   * @param {string} name new category name
+   * @returns
+   */
+  const addList = list => {
+    if (!list) return;
+
+    setBills(b => {
+      if (b[list]) return b;
+
+      return { ...b, [list]: [] };
+    });
   };
 
   /**
@@ -32,6 +47,19 @@ export default function useBills() {
   const removeBill = target => {
     setBills(b => {
       for (const key in b) if (Array.isArray(b[key])) b[key] = b[key].filter(item => item.id !== target);
+
+      return b;
+    });
+  };
+
+  /**
+   * Remove a list
+   *
+   * @param {string} list name of list to delete
+   */
+  const removeList = list => {
+    setBills(b => {
+      delete b[list];
 
       return b;
     });
@@ -60,15 +88,14 @@ export default function useBills() {
   return useMemo(
     () => ({
       bills,
-      total:
-        Object.keys(bills).reduce((t, k) => {
-          t += bills[k].reduce((it, itm) => (it += itm.value), 0) || 0;
-        }, 0) || 0,
+      total: Object.keys(bills).reduce((t, k) => (t += bills[k].reduce((it, itm) => (it += itm.value), 0)), 0),
       addBill,
+      addList,
       removeBill,
+      removeList,
       updateBill,
     }),
-    [bills, addBill, removeBill, updateBill]
+    [bills, addBill, addList, removeBill, removeList, updateBill]
   );
 }
 
