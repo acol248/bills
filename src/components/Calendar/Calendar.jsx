da83c69f8ad003c9679dc63872dfefd2b9890b9d
acol248 <acol248@gmail.com>
2024-06-05 22:19:31 +0100

    💄 Updated Calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,27 +1,16 @@
-import { useCallback, useRef, useState } from "react";
-import { useLayer } from "react-laag";
-import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
+import { useCallback, useState } from "react";
 import { Calendar as AriaCalendar, Button, Heading, CalendarCell, CalendarGrid } from "react-aria-components";
 
+// components
+import Modal from "../Modal";
+
 // styles
+import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
 import maps from "./Calendar.module.scss";
 const mc = mapClassesCurried(maps, true);
 
 export default function Calendar({ className, selectedDate, onChange }) {
-  const buttonRef = useRef(null);
-
   const [isOpen, setIsOpen] = useState(false);
-
-  const { triggerProps, layerProps, renderLayer } = useLayer({
-    isOpen,
-    onOutsideClick: () => setIsOpen(false),
-    onDisappear: () => setIsOpen(false),
-    auto: true,
-    overflowContainer: true,
-    triggerOffset: 5,
-    containerOffset: 10,
-    container: () => buttonRef.current.parentElement,
-  });
 
   const buttonClassList = useClassList(
     { defaultClass: "calendar", className, maps, string: true },
@@ -29,40 +18,42 @@ export default function Calendar({ className, selectedDate, onChange }) {
   );
   const menuClassList = useClassList({ defaultClass: "calendar-menu", maps, string: true });
 
+  /**
+   * Close menu and trigger params change when calendar change
+   *
+   * @param {object} v calendar value
+   */
+  const handleOnChange = v => {
+    onChange(v);
+    setTimeout(() => setIsOpen(false), 0);
+  };
+
   return (
     <>
-      <button
-        {...triggerProps}
-        type="button"
-        className={buttonClassList}
-        ref={r => {
-          buttonRef.current = r;
-          triggerProps.ref(r);
-
-          return r;
-        }}
-        onClick={() => setIsOpen(o => !o)}
-      >
+      <button type="button" className={buttonClassList} onClick={() => setIsOpen(true)}>
         {selectedDate ? new Date(selectedDate)?.toLocaleDateString() || "" : "No date selected"}
       </button>
 
-      {isOpen &&
-        renderLayer(
-          <div {...layerProps} className={menuClassList}>
-            <AriaCalendar className={mc("calendar-menu__calendar")} onChange={onChange}>
-              <header className={mc("calendar-menu__header")}>
-                <Button slot="previous">◀</Button>
-                <Heading />
-                <Button slot="next">▶</Button>
-              </header>
-              <div className={mc("calendar-menu__inner")}>
-                <CalendarGrid>
-                  {date => <CalendarCell className={mc("calendar-menu__cell")} date={date} />}
-                </CalendarGrid>
-              </div>
-            </AriaCalendar>
+      <Modal className={menuClassList} title="Calendar" open={isOpen} onClose={() => setIsOpen(false)}>
+        <AriaCalendar className={mc("calendar-menu__calendar")} onChange={handleOnChange}>
+          <header className={mc("calendar-menu__header")}>
+            <Button slot="previous">
+              <svg viewBox="0 -960 960 960">
+                <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+              </svg>
+            </Button>
+            <Heading />
+            <Button slot="next">
+              <svg viewBox="0 -960 960 960">
+                <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+              </svg>
+            </Button>
+          </header>
+          <div className={mc("calendar-menu__inner")}>
+            <CalendarGrid>{date => <CalendarCell className={mc("calendar-menu__cell")} date={date} />}</CalendarGrid>
           </div>
-        )}
+        </AriaCalendar>
+      </Modal>
     </>
   );
 }
