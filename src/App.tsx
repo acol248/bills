eaa191f4f37c5ae8676a3bbb00e1af4dceb5254e
acol248 <alex.collyer@acol.dev>
2024-08-04 12:57:33 +0100

    🎨 ✨ Fixes and general improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // hooks
 import useData, { DataContext } from "./hooks/useData";
@@ -9,31 +9,19 @@ import BaseNavigation from "./components/BaseNavigation";
 
 // parts
 import UpComing from "./parts/UpComing";
+import AddItem from "./parts/AddItem";
+import Settings from "./parts/Settings";
 
 // styles
 import "./App.module.scss";
-import Settings from "./parts/Settings";
-import {
-  generateCSSVariables,
-  elementsDark,
-  elementsLight,
-  theme,
-} from "./interface";
-import AddItem from "./parts/AddItem";
+import { generateCSSVariables, elementsDark, elementsLight, theme } from "./interface";
 
 export default function App() {
   const data = useData();
   const settings = useSettings();
 
-  const router = createBrowserRouter([
-    { path: "/", element: <UpComing /> },
-    { path: "/settings", element: <Settings /> },
-  ]);
-
   const themeStyles = generateCSSVariables(theme);
-  const elementsTheme = generateCSSVariables(
-    settings.theme === "light" ? elementsLight : elementsDark
-  );
+  const elementsTheme = generateCSSVariables(settings.theme === "light" ? elementsLight : elementsDark);
 
   return (
     <SettingsContext.Provider value={settings}>
@@ -41,11 +29,16 @@ export default function App() {
         <style dangerouslySetInnerHTML={{ __html: themeStyles }} />
         <style dangerouslySetInnerHTML={{ __html: elementsTheme }} />
 
-        <RouterProvider router={router} />
+        <BrowserRouter basename={import.meta.env.VITE_BASENAME}>
+          <Routes>
+            <Route index element={<UpComing />} />
+            <Route path="settings" element={<Settings />} />
+          </Routes>
 
-        <AddItem />
+          <AddItem />
 
-        <BaseNavigation />
+          <BaseNavigation />
+        </BrowserRouter>
       </DataContext.Provider>
     </SettingsContext.Provider>
   );
