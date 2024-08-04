033969b2a269090469abf0477531cac1eac664f4
acol248 <alex.collyer@acol.dev>
2024-08-04 12:57:01 +0100

    🚧 Minor update to Settings for dev

--- src/parts/Settings/Settings.tsx
+++ src/parts/Settings/Settings.tsx
@@ -1,18 +1,27 @@
-import useClassList from "@blocdigital/useclasslist";
+import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
+import maps from "./Settings.module.scss";
+const mc = mapClassesCurried(maps, true);
+
 import Toggle from "../../interface/Toggle";
 import { useContext } from "react";
 import { SettingsContext } from "../../hooks/useSettings";
 
 export default function Settings() {
-  const { theme, toggleTheme } = useContext(SettingsContext);
+  const { theme, forceScale, toggleTheme, toggleForceScale } = useContext(SettingsContext);
 
-  const classList = useClassList({ defaultClass: "settings" }) as string;
+  const classList = useClassList({ defaultClass: "settings", maps, string: true });
 
   return (
     <div className={classList}>
       <Toggle checked={theme === "dark"} onChange={() => toggleTheme()}>
         Dark Mode
       </Toggle>
+
+      <Toggle checked={forceScale} onChange={() => toggleForceScale()}>
+        Force Increased Text Scale
+      </Toggle>
+
+      <p className={mc('settings__version')}>Version: {__APP_VERSION__}</p>
     </div>
   );
 }
