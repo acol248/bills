7a40c8bb4b90d965effba582d97b24617b8d691b
acol248 <alex.collyer@acol.dev>
2024-08-18 11:06:37 +0100

    💄 ✨ Updated ListItem component

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -3,6 +3,8 @@ import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
 import maps from "./ListItem.module.scss";
 import { addDateSuffix } from "../../helpers/itemHelpers";
 import { formatCurrency } from "../../helpers/formatCurrency";
+import Button from "../../interface/Button";
+import { useState } from "react";
 const mc = mapClassesCurried(maps, true) as (c: string) => string;
 
 // types
@@ -11,19 +13,30 @@ interface ItemProps {
   label: string;
   value: number;
   date: Date;
+  onEdit: () => void;
+  onDelete: () => void;
 }
 
-export default function ListItem({ className, label, value, date }: ItemProps) {
+export default function ListItem({ className, label, value, date, onEdit, onDelete }: ItemProps) {
+  const [open, setOpen] = useState<boolean>(false);
+
   const classList = useClassList({ defaultClass: "list-item", className, maps, string: true }) as string;
 
   return (
-    <div className={classList}>
-      <div className={mc("list-item__left")}>
-        <p className={mc("list-item__label")}>{label}</p>
-        <p className={mc("list-item__value")}>{formatCurrency(value)}</p>
-      </div>
+    <div className={classList} aria-checked={open}>
+      <button className={mc("list-item__content")} type="button" role="switch" onClick={() => setOpen(o => !o)}>
+        <div className={mc("list-item__left")}>
+          <p className={mc("list-item__label")}>{label}</p>
+          <p className={mc("list-item__value")}>{formatCurrency(value)}</p>
+        </div>
 
-      <p className={mc("list-item__date")}>{addDateSuffix(new Date(date).getDate())}</p>
+        <p className={mc("list-item__date")}>{addDateSuffix(new Date(date).getDate())}</p>
+      </button>
+
+      <div className={mc("list-item__controls")}>
+        <Button onClick={onEdit}>Edit</Button>
+        <Button onClick={onDelete}>Delete</Button>
+      </div>
     </div>
   );
 }
