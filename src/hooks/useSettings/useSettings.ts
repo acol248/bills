d78510a220f57df5057db087b96aadc397ee7379
acol248 <alex.collyer@acol.dev>
2024-09-01 18:26:20 +0100

    ✨ Added authentication setup and verify functions

--- src/hooks/useSettings/useSettings.ts
+++ src/hooks/useSettings/useSettings.ts
@@ -2,6 +2,7 @@ import { createContext, useCallback, useLayoutEffect, useMemo, useState } from "
 
 // types
 import useLocalStorage from "@blocdigital/uselocalstorage";
+import { bufferToString } from "../../helpers/format";
 
 type Settings = {
   theme: "light" | "dark";
@@ -10,18 +11,26 @@ type Settings = {
 };
 
 interface UseSettings {
+  authCheck: boolean;
+  authenticated: boolean;
   theme: "light" | "dark";
   forceScale: boolean;
   scale: number;
   toggleTheme: (theme?: "light" | "dark") => void;
   toggleForceScale: (state?: boolean) => void;
   updateScale: (scale: number) => void;
+  setupAuthentication: (code: string) => Promise<boolean>;
+  verifyAuthentication: (code?: string, genericCheck?: boolean) => Promise<boolean>;
+  deAuthenticate: () => void;
 }
 
 export default function useSettings(): UseSettings {
   const storage = useLocalStorage("local");
+  const session = useLocalStorage("session");
 
   const [data, setData] = useState<Settings>({ theme: "light", scale: 1.25, forceScale: false });
+
+  const [authenticated, setAuthenticated] = useState(false);
 
   const toggleTheme = useCallback<UseSettings["toggleTheme"]>(theme => {
     setData(d => {
@@ -49,6 +58,51 @@ export default function useSettings(): UseSettings {
     setData(d => ({ ...d, scale }));
   }, []);
 
+  const setupAuthentication = useCallback<UseSettings["setupAuthentication"]>(async code => {
+    return new Promise(async (resolve, reject) => {
+      if (!code) reject(false);
+
+      const encoded = new TextEncoder().encode(code);
+      const hash = await window.crypto.subtle.digest("sha-256", encoded);
+
+      if (!hash) reject(false);
+
+      storage.set("check", bufferToString(hash));
+      resolve(true);
+    });
+  }, []);
+
+  const verifyAuthentication = useCallback<UseSettings["verifyAuthentication"]>(async (code, genericCheck) => {
+    return new Promise(async (resolve, reject) => {
+      if (genericCheck && storage.get("check")) {
+        const c_encoded = new TextEncoder().encode(code);
+        const c_hash = await window.crypto.subtle.digest("sha-256", c_encoded);
+
+        if (bufferToString(c_hash) !== storage.get("check")) return reject(false);
+
+        return resolve(true);
+      }
+
+      if (!storage.get("check")) {
+        setAuthenticated(true);
+
+        reject(false);
+      }
+
+      const c_encoded = new TextEncoder().encode(code);
+      const c_hash = await window.crypto.subtle.digest("sha-256", c_encoded);
+
+      if (bufferToString(c_hash) !== storage.get("check")) reject(false);
+
+      const s_encoded = new TextEncoder().encode(bufferToString(c_hash));
+      const s_hash = await window.crypto.subtle.digest("sha-256", s_encoded);
+
+      session.set("check", bufferToString(s_hash));
+      setAuthenticated(true);
+      resolve(true);
+    });
+  }, []);
+
   // adjust scale style
   useLayoutEffect(() => {
     document.body.style.setProperty("--core-scale", data.forceScale ? data.scale.toString() : "1");
@@ -69,22 +123,43 @@ export default function useSettings(): UseSettings {
 
   return useMemo(
     () => ({
+      authCheck: Boolean(storage.get("check")),
+      authenticated,
       theme: data.theme,
       forceScale: data.forceScale,
       scale: data.scale,
       toggleTheme,
       toggleForceScale,
       updateScale,
+      setupAuthentication,
+      verifyAuthentication,
+      deAuthenticate: () => setAuthenticated(false),
     }),
-    [data.theme, data.forceScale, data.scale, toggleTheme, toggleForceScale]
+    [
+      storage,
+      authenticated,
+      data.theme,
+      data.forceScale,
+      data.scale,
+      toggleTheme,
+      toggleForceScale,
+      updateScale,
+      setupAuthentication,
+      verifyAuthentication,
+    ]
   );
 }
 
 export const SettingsContext = createContext<UseSettings>({
+  authCheck: false,
+  authenticated: false,
   theme: "light",
   forceScale: true,
   scale: 1,
   toggleTheme: () => {},
   toggleForceScale: () => {},
   updateScale: () => {},
+  setupAuthentication: async () => false,
+  verifyAuthentication: async () => false,
+  deAuthenticate: () => {},
 });
