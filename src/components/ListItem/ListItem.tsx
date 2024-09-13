61bc671309581baeb88763d0ac68f9f07fedf35c
acol248 <alex.collyer@acol.dev>
2024-09-14 00:55:42 +0100

    💄 Update to ListItem

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
+// components
+import Button from "../../interface/Button";
+
+// helpers
+import { formatCurrency } from "../../helpers/format";
+
 // styles
 import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
 import maps from "./ListItem.module.scss";
-import { addDateSuffix } from "../../helpers/itemHelpers";
-import { formatCurrency } from "../../helpers/format";
-import Button from "../../interface/Button";
-import { useState } from "react";
 const mc = mapClassesCurried(maps, true) as (c: string) => string;
 
 // types
@@ -17,20 +21,28 @@ interface ItemProps {
   onDelete: () => void;
 }
 
-export default function ListItem({ className, label, value, date, onEdit, onDelete }: ItemProps) {
+export default function ListItem({ className, label, value, onEdit, onDelete }: ItemProps) {
   const [open, setOpen] = useState<boolean>(false);
 
   const classList = useClassList({ defaultClass: "list-item", className, maps, string: true }) as string;
 
   return (
     <div className={classList} aria-checked={open}>
-      <button className={mc("list-item__content")} type="button" role="switch" onClick={() => setOpen(o => !o)}>
+      <button
+        className={mc("list-item__content")}
+        type="button"
+        role="switch"
+        aria-selected={open}
+        onClick={() => setOpen(o => !o)}
+      >
         <div className={mc("list-item__left")}>
           <p className={mc("list-item__label")}>{label}</p>
           <p className={mc("list-item__value")}>{formatCurrency(value)}</p>
         </div>
 
-        <p className={mc("list-item__date")}>{addDateSuffix(new Date(date).getDate())}</p>
+        <svg viewBox="0 -960 960 960">
+          <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+        </svg>
       </button>
 
       <div className={mc("list-item__controls")}>
