0ef1dcc616834aadbecdb5287c7d06e4d411d1c6
acol248 <alex.collyer@acol.dev>
2024-10-01 22:38:39 +0100

    🔧 Adjust config for new deployment location

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/app/bills-beta/",
+  base: "/app/bills",
   plugins: [
     react(),
     VitePWA({
