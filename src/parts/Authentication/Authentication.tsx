f985b0e7954ff54837d2f4bc609da9fe1568508c
acol248 <alex.collyer@acol.dev>
2024-10-01 20:07:32 +0100

    🐛 Basic error checking

--- src/parts/Authentication/Authentication.tsx
+++ src/parts/Authentication/Authentication.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -16,11 +16,20 @@ const mc = mapClassesCurried(maps, true);
 export default function Authentication() {
   const { authCheck, verifyAuthentication, vibrate } = useContext(SettingsContext);
 
+  const [wrong, setWrong] = useState<boolean>(false);
   const [passCode, setPassCode] = useState<string | undefined>(undefined);
 
   const navigate = useNavigate();
 
-  const classList = useClassList({ defaultClass: "auth", maps, string: true });
+  const classList = useClassList(
+    { defaultClass: "auth", maps, string: true },
+    useCallback(
+      (c: string[]) => {
+        wrong && c.push("auth--fail");
+      },
+      [wrong]
+    )
+  );
 
   /**
    * Handle a keypress event from the Keypad component
@@ -28,6 +37,8 @@ export default function Authentication() {
    * @param code key entered
    */
   const press = (code: string | undefined) => {
+    if (wrong) return;
+
     vibrate();
     setPassCode((p: string | undefined) => {
       if (code === "backspace" && p) return p.substring(0, p.length - 1);
@@ -57,7 +68,16 @@ export default function Authentication() {
             onChange={press}
             onConfirm={() =>
               vibrate({
-                callback: () => verifyAuthentication(passCode).then(() => navigate("/up-coming", { replace: true })),
+                callback: () =>
+                  verifyAuthentication(passCode)
+                    .then(() => navigate("/up-coming", { replace: true }))
+                    .catch(() => {
+                      setWrong(true);
+                      setTimeout(() => {
+                        setWrong(false);
+                        setPassCode(undefined);
+                      }, 1000);
+                    }),
               })
             }
           />
