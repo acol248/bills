bcab8784ee4c14dd5245dd696f71d73c63699b97
acol248 <alex.collyer@acol.dev>
2025-03-06 23:22:41 +0000

    ✨ Updated Select component

--- src/interface/Select/Select.d.ts
+++ src/interface/Select/Select.d.ts
@@ -10,6 +10,10 @@ interface Props extends SelectProps {
   label: string;
   items: Array<Item>;
   placeholder?: string;
+  name?: string;
+  defaultValue?: string;
+  value?: string;
+  onValueChange?: (value: string) => void;
 }
 
 export type { Props, Item };
